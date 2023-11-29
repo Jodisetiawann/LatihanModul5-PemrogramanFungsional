bf34d2b90a65dd9174d12807c5114ba3a313f834
Jodisetiawann <110955272+Jodisetiawann@users.noreply.github.com>
2023-11-29 08:12:55 +0700

    Update percobaan5.py

--- LatihanPraktikum-Modul5/percobaan5.py
+++ LatihanPraktikum-Modul5/percobaan5.py
@@ -3,22 +3,25 @@ import numpy as np
 from numpy import mean, std
 from scipy.stats import norm
 
-
 sample = np.random.normal(loc=50, scale=5, size=1000)
-# sample_mean = mean(sample)
-# sample_std = std(sample)
-# print('Mean=%.3f \nStandard Deviation=%.3f' % (sample_mean, sample_std))
-# dist = norm(sample_mean, sample_std)
-# dist
-# values = [value for value in range(30, 70)]
-# probabilitas = [dist.pdf(value) for value in values]
-# probabilitas
-print(sample)
 
+sample_mean = mean(sample)
+sample_std = std(sample)
+print('Mean=%.3f \nStandard Deviation=%.3f' % (sample_mean, sample_std))
+
+dist = norm(sample_mean, sample_std)
+
+values = np.linspace(30, 70, 1000)
+
+probabilities = dist.pdf(values)
+
+plt.figure(figsize=(8, 6))
+plt.hist(sample, bins=10, density=True, alpha=0.6, color='blue', label='Sample Histogram')
+plt.plot(values, probabilities, color='r', label='Distribusi Normal')
 
 plt.title('Histogram Sample Data Normal')
-plt.xlabel('Nilai')
-plt.ylabel('Frekuensi Relatif')
-plt.figure(figsize=(5,4))
-plt.hist(sample, bins=10, density=True)
+plt.xlabel('Value')
+plt.ylabel('Relative Frequency')
+
+plt.legend()
 plt.show()
